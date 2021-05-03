88112ba6178f209c8fbd7a15570e592e830e8110
Håkon Johansen <79876326+haakonjohansen@users.noreply.github.com>
2021-05-03 13:45:49 +0200

    javascript syntax
    here i added the necessary, not finished and needs grammar check and formatting

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+if,else,while,do,for,switch,case,default,var,let,const // these are keywords. they describe logic or property
+"hello",24,true,false //they are literals. they can be described as values
+ ;(){}[] //these are seperator. they describes where or when to stop. 
+my_name, my_variable // these are indentifers. they are the names of whatever type you specify and they hold that data 
++<>- // these are operators. they add or tell the program do something
+// this is a comment and i will be ignored as executable only text
+  
+var x = 5;
+
+keyword - indentifer - operator - literal - seperator
+
+numeric-literal: 123456789
+string-literal: "hello"
